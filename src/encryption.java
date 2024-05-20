15f3c3fbe941abebf3843fc6e23b7e206998e6f8
Yllka Kastrati <117120519+Yllka5@users.noreply.github.com>
2024-05-20 19:20:45 +0200

    Add files via upload

---
+++ src/encryption.java
@@ -0,0 +1,31 @@
+
+import javax.crypto.*;
+        import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+
+public class EncryptionUtil {
+
+    public static byte[] encryptRSA(byte[] data, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decryptRSA(byte[] data, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] encryptAES(byte[] data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+
+    public static byte[] decryptAES(byte[] data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
+}
